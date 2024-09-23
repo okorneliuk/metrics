c9cecf486f868a1f3ddb6e6bcbad6d814a686f13
Maxim Smakouz <m.smakouz@gmail.com>
2024-09-23 17:03:41 +0300

    Fix Psalm errors (#16)
    * Fix psalm issues, strict_types
    
    * Fix phpunit tests
    
    * Revert the use of the withServicePrefix method
    
    * Improve constant type

--- src/AbstractMetrics.php
+++ src/AbstractMetrics.php
@@ -1,13 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Spiral\RoadRunner\Metrics;
 
 use Spiral\Goridge\RPC\RPCInterface;
 
 abstract class AbstractMetrics implements MetricsInterface
 {
+    protected readonly RPCInterface $rpc;
+
 	/**
-	 * @var string
+	 * @var non-empty-string
 	 */
     protected const SERVICE_NAME = 'metrics';
+
+    public function __construct(RPCInterface $rpc)
+    {
+        $this->rpc = $rpc->withServicePrefix(static::SERVICE_NAME);
+    }
 }
